f373ffe953b2d81449a1bda2a2fbc4addce654d7
harsha-1807 <harshavardhan.sr@kalvium.community>
2024-03-08 12:24:41 +0530

    made using functinal component

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,71 +1,43 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "./Carousel.css";
 import { images } from "../data/CarouselData";
-// you can explore more - and check as how to use materiul ui
-import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos'
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
-import { RoundedCornerSharp } from "@mui/icons-material";
 
-// implement the class below
-class Carousel extends Component {
-  constructor(){
-    super()
-    // set an initial state
-    this.state = {
-        currIndex:0
+const Carousel = () => {
+  const [currIndex, setCurrIndex] = useState(0);
+
+  const increaseCount = () => {
+    if (currIndex >= images.length - 1) {
+      setCurrIndex(0);
+    } else {
+      setCurrIndex(currIndex + 1);
     }
-  }
+  };
 
-  increasecount =()=>{
-    console.log("increase count")
-    if (this.state.currIndex >= images.length - 1){
-        this.setState({
-            currIndex:0
-        })
-    }else{
-        this.setState({
-            currIndex : this.state.currIndex + 1
-
-        })
-        
+  const decreaseCount = () => {
+    if (currIndex === 0) {
+      setCurrIndex(images.length - 1);
+    } else {
+      setCurrIndex(currIndex - 1);
     }
-  }
-  decreasecount =()=>{
-    console.log("decrease count")
-    // this.setState({
-    //     currIndex : this.state.currIndex - 1
-    // })
-    if (this.state.currIndex == 0 ){
-        this.setState({
-            currIndex:2
-        })
-    }else{
-        this.setState({
-            currIndex : this.state.currIndex - 1
+  };
 
-        })
-        
-    }
-  }
-
-
-  render(){
-    return <>
-        {/* <h1>Carousel</h1> */}
-        <div className="carouselContainer flex">
-            <div className="leftarrow arrowdiv flex" onClick={this.decreasecount}>
-                <ArrowBackIosIcon  className="chevron"/>
-            </div>
-            <h2 className="title">{images[this.state.currIndex].title}</h2>
-            <img src={images[this.state.currIndex].img} alt="" />
-            <h4 className="text">{images[this.state.currIndex].subtitle}</h4>
-
-        <div className="rightarrow arrowdiv flex" onClick={this.increasecount}>
-                <  ArrowForwardIosIcon     className="chevron"/>
+  return (
+    <>
+      <div className="carouselContainer flex">
+        <div className="leftarrow arrowdiv flex" onClick={decreaseCount}>
+          <ArrowBackIosIcon className="chevron" />
         </div>
+        <h2 className="title">{images[currIndex].title}</h2>
+        <img src={images[currIndex].img} alt="" />
+        <h4 className="text">{images[currIndex].subtitle}</h4>
+        <div className="rightarrow arrowdiv flex" onClick={increaseCount}>
+          <ArrowForwardIosIcon className="chevron" />
         </div>
+      </div>
     </>
-  }
-}
+  );
+};
 
 export default Carousel;
